487d08d6ebd311ce5c9ca8f87086074da1a3b949
Ella-ki <ellakim8523@gmail.com>
2024-02-06 02:26:41 +0900

    header

--- src/main/java/com/gowine/config/WebMvcConfig.java
+++ src/main/java/com/gowine/config/WebMvcConfig.java
@@ -1,9 +1,11 @@
 package com.gowine.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.extras.springsecurity6.dialect.SpringSecurityDialect;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
