2ac497541b1575eba25f15e0449d29e9336f2a78
Ella-ki <ellakim8523@gmail.com>
2024-02-25 01:44:40 +0900

    Redis 사용 설정 완료 및 테스트 완료

---
+++ src/main/java/com/gowine/redis/RedisConfig.java
@@ -0,0 +1,43 @@
+package com.gowine.redis;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/************
+ * @info : Redis Repository Config 클래스
+ * @name : RedisRepositoryConfig
+ * @version : 1.0.0
+ * @Description : Lettuce 사용(비동기 요청 처리), RedisRepository 방식.
+ ************/
+@Configuration
+@RequiredArgsConstructor
+@EnableRedisRepositories
+public class RedisConfig {
+    private final RedisProperties redisProperties;
+
+    // lettuce
+    // RedisProperties로 host, port 연결
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+    }
+
+    // Redis template
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+
+        return redisTemplate;
+    }
+}
