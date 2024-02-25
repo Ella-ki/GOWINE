d606d5a0522eec1d92b28d76120dbd5fa74fe07e
Ella-ki <ellakim8523@gmail.com>
2024-02-25 23:08:15 +0900

    휴대폰 인증 수정 필요

--- src/main/java/com/gowine/controller/LoginController.java
+++ src/main/java/com/gowine/controller/LoginController.java
@@ -12,6 +12,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Random;
 
 @Slf4j
 @Controller
@@ -58,4 +61,5 @@ public class LoginController {
         model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
         return "member/loginForm";
     }
+
 }
